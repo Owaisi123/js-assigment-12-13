203f608c80d879d41eb279da725967bd3d671180
Owaisi123 <165272983+Owaisi123@users.noreply.github.com>
2024-09-18 18:27:22 +0500

    Add files via upload

---
+++ chapter 12 - 13/index.js
@@ -0,0 +1,100 @@
+// var input = prompt("Enter a character:"); // For browser environments
+
+// if (input === "") {
+//     document.write("Please enter a character.");
+// } else {
+//     var char = input[0];
+
+//     if (char >= '0' && char <= '9') {
+//         document.write(char + " is a number.");
+//     } else if (char >= 'A' && char <= 'Z') {
+//         document.write(char + " is an uppercase letter.");
+//     } else if (char >= 'a' && char <= 'z') {
+//         document.write(char + " is a lowercase letter.");
+//     } else {
+//         document.write(char + " is neither a number nor a letter.");
+//     }
+// }
+
+// Qno:2
+
+// var num1 = (prompt("Enter the first integer:")); // For browser environments
+// var num2 = (prompt("Enter the second integer:"));
+
+// if (num1 === num2) {
+//     document.write("Both integers are equal.");
+// } else {
+//     var larger = (num1 > num2) ? num1 : num2;
+//     document.write("The larger integer is: " + larger);
+// }
+
+
+// Qno:3
+
+// var number = parseFloat(prompt("Enter a number:")); // For browser environments
+
+// if (number > 0) {
+//     document.write("The number is positive.");
+// } else if (number < 0) {
+//     document.write("The number is negative.");
+// } else {
+//     document.write("The number is zero.");
+// }
+
+//Qno:4;
+// var user=prompt("Enter alphabet?");
+// var alphabet="a";
+// var alphabet1='e';
+// var alphabet2='i';
+// var alphabet3='o';
+// var alphabet4='u'
+// if (user == alphabet || user == alphabet1 || user == alphabet2 || user == alphabet3 || user == alphabet4) {
+//     alert("this is vowel")
+// } else {
+//     alert('false');
+// }
+
+
+//Qno:5;
+
+// var password="9123@@771$";
+// alert("Enter your password??")
+
+// var user=prompt("Enter the password?");
+// if (user == password) {
+//     alert("Correct!")
+// } else {
+//     alert("Wrong password! try again")
+// }
+
+//Qno:6;
+
+// var greeting = "good day";
+// var greeting1 = 'good evening';
+// var user=prompt("Enter hour")
+// if (user <= 12) {
+//     alert("Good day!")
+// } else if (user >= 12) {
+//     alert("Good evening!")
+    
+// }
+
+// Qno:7;
+
+// var time=1900;
+// var time1=2000;
+// var time2=2100;
+// var time3=2200;
+// var time4=2300;
+// var user=prompt("Enter NO and check time? 1900 , 2000 , 2100 , 2200 , 2300")
+// if (user == time  ) {
+//     alert("7pm")
+// } else if (user == time1) {
+//    alert("8pm"); 
+// }else if(user == time2){
+//     alert("9pm")
+// }else if(user == time3){
+//     alert("10pm")
+// }else if (user == time){
+//     alert("11pm")
+// }
